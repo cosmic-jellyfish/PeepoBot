415c28577d815363ba10e0813c5fb6dea9a4b1f3
peepoboi <41272291+peepoboi@users.noreply.github.com>
2020-08-14 12:22:03 +1000

    Create confused.js

---
+++ archive/confused.js
@@ -0,0 +1,36 @@
+/*
+* Command: $confused
+* Author: Cake#0585
+* Description: i is mega confusion
+*/
+
+const Discord = require('discord.js');
+const { Attachment } = require("discord.js");
+const Idiot = require("idiotic-api");
+
+exports.exec = async (client, message, args, API, user) => {
+client.API = new Idiot.Client("xxxxxxxxxxxxxx", { dev: true });
+    // Fires Error message that the command wasn't ran correctly.
+    if (args == '') {
+        return client.emit('commandUsage', message, this.help);
+    }
+    // Fires Error message that the command wasn't ran correctly.
+
+	    let url1 = message.author.displayAvatarURL, url2 = message.mentions.users.first().displayAvatarURL;
+
+
+	message.channel.send('`Processing image`.')
+	.then(msg => {
+    msg.delete(5000)
+	})
+    message.channel.startTyping();
+    let bs = await client.API.confused(url1, url2);
+    
+    await message.channel.stopTyping();
+    return message.channel.send({ files: [{ attachment: bs }] });
+	
+};
+
+
+
+/* * * * */
